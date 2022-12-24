18c94f154d09e8cc065fd0830b0cd0590d86aae3
victicor1989 <losdelsur1947@hotmail.com>
2022-12-23 22:23:02 -0500

    v1.5, cambios significativos

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const buttonThree = document.querySelector('#three')
 const buttonFour = document.querySelector('#four')
 const buttonFive = document.querySelector('#five')
 const containerParagraph = document.querySelector('#container-paragraph')
+const containerContent = document.querySelector('#container-paragraph')
 let buttons = [];
 let ButtonsOption;
 
-containerMain.style.display = 'none'
+containerContent.style.display = 'none'
 submitButton.addEventListener('click', showDesktopThankState)
 buttonOne.addEventListener('click', selectCalification)
 buttonTwo.addEventListener('click', selectCalification)
@@ -21,17 +22,31 @@ buttons.push(buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive)
 console.log(buttons);
 
 function selectCalification() {
-    ButtonsOption.forEach(() => {
+    if (buttons == buttonOne) {
+        containerParagraph.innerHTML =+ 'You select 1 out of 5'
+    } else if (buttons == buttonTwo) {
+        containerParagraph.innerHTML =+ 'You select 2 out of 5'
+    } else if (buttons == buttonThree) {
+        containerParagraph.innerHTML =+ 'You select 3 out of 5'
+    } else if (buttons == buttonFour) {
+        containerParagraph.innerHTML =+ 'You select 4 out of 5'
+    } else if (buttons == buttonFive) {
+        containerParagraph.innerHTML =+ 'You select 5 out of 5'
+    } else {
+        containerParagraph.innerHTML =+ 'Escoge bien puto'
+    }
+    buttons.forEach((buttons) => {
             ButtonsOption = `
             <p>You selected ${buttons.ButtonsOption} out of 5</p>
             `
-
+            containerParagraph.innerHTML =+ ButtonsOption
         }) 
+
 }
 
 
 function showDesktopThankState() {
     containerMain.style.display = 'none'
-    showDesktopThankState()
+    selectCalification()
 }
 
