5144c4cbda4ce3fc79c4c56f55a998b1f5b6cc1c
victicor1989 <losdelsur1947@hotmail.com>
2022-12-26 17:49:14 -0500

    v1.6, se agrega button de reload page y estilos css

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Const and lets
+
 const containerMain = document.querySelector('.main-container')
 const submitButton = document.querySelector('#btn-submit')
 const buttonOne = document.querySelector('#one')
@@ -5,48 +7,37 @@ const buttonTwo = document.querySelector('#two')
 const buttonThree = document.querySelector('#three')
 const buttonFour = document.querySelector('#four')
 const buttonFive = document.querySelector('#five')
-const containerParagraph = document.querySelector('#container-paragraph')
 const containerContent = document.querySelector('#container-paragraph')
-let buttons = [];
-let ButtonsOption;
+const containerParagraph = document.querySelector('#paragraph')
+const reloadButton = document.querySelector('#reset')
+let botones = [];
 
 containerContent.style.display = 'none'
+
+//Add Event Listener
+
 submitButton.addEventListener('click', showDesktopThankState)
-buttonOne.addEventListener('click', selectCalification)
-buttonTwo.addEventListener('click', selectCalification)
-buttonThree.addEventListener('click', selectCalification)
-buttonFour.addEventListener('click', selectCalification)
-buttonFive.addEventListener('click', selectCalification)
+buttonOne.addEventListener('click', readButton)
+buttonTwo.addEventListener('click', readButton)
+buttonThree.addEventListener('click', readButton)
+buttonFour.addEventListener('click', readButton)
+buttonFive.addEventListener('click', readButton)
+reloadButton.addEventListener('click', reloadPage)
 
-buttons.push(buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive)
-console.log(buttons);
-
-function selectCalification() {
-    if (buttons == buttonOne) {
-        containerParagraph.innerHTML =+ 'You select 1 out of 5'
-    } else if (buttons == buttonTwo) {
-        containerParagraph.innerHTML =+ 'You select 2 out of 5'
-    } else if (buttons == buttonThree) {
-        containerParagraph.innerHTML =+ 'You select 3 out of 5'
-    } else if (buttons == buttonFour) {
-        containerParagraph.innerHTML =+ 'You select 4 out of 5'
-    } else if (buttons == buttonFive) {
-        containerParagraph.innerHTML =+ 'You select 5 out of 5'
-    } else {
-        containerParagraph.innerHTML =+ 'Escoge bien puto'
-    }
-    buttons.forEach((buttons) => {
-            ButtonsOption = `
-            <p>You selected ${buttons.ButtonsOption} out of 5</p>
-            `
-            containerParagraph.innerHTML =+ ButtonsOption
-        }) 
-
-}
-
+botones.push(buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive)
+botones.forEach(readButton)
 
 function showDesktopThankState() {
     containerMain.style.display = 'none'
-    selectCalification()
+    containerContent.style.display = 'flex'
+    readButton()
 }
 
+function readButton(item, index, arr) {
+    containerParagraph.innerHTML = `You selected ${4} out of 5`;
+    console.log('a[' + index + ']' + item);
+}
+
+function reloadPage() {
+    location.reload()
+}
